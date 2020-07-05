c9a91d651578c457753b3d00183b65dc70ea4dac
Jeremy <jeremyhou07@gmail.com>
2020-07-05 19:57:57 -0400

    Baseline

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -8,13 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    let answers = ["It is certain", "It is decidedly   so", "Without a doubt", "Yes – definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you    now", "Cannot predict    now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+     
+    let noInputAnswers = ["I can't read your mind", "No question, no answer", "Type in a      question"]
+    
+    let animationDuration = 1.5
+
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var questionBox: UITextField!
+    
+    @IBAction func askButtonClicked(_ sender: Any) {
+        if questionBox.text != ""{
+            if let randomAnswer = answers.randomElement(){
+                fadeViewOut(view: answerLabel, delay: 0)
+                print(randomAnswer)
+                answerLabel.text = randomAnswer
+                fadeViewIn(view: answerLabel, delay: 0)
+            }
+        }else {
+            if let randomAnswer = noInputAnswers.randomElement(){
+                fadeViewOut(view: answerLabel, delay: 0)
+                print(randomAnswer)
+                answerLabel.text = randomAnswer
+                fadeViewIn(view: answerLabel, delay: 0)
+            }
+        }
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+       
+    func hideKeyboard() {
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func fadeViewIn(view : UIView, delay: TimeInterval) {
+        UIView.animate(withDuration: animationDuration, delay: delay, options: .curveEaseIn, animations: {
+            view.alpha = 1
+        }, completion: nil)
+    }
+    
+    func fadeViewOut(view : UIView, delay: TimeInterval) {
+        UIView.animate(withDuration: animationDuration, delay: delay, options: .curveEaseIn, animations: {
+            view.alpha = 0
+        }, completion: nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.hideKeyboard()
+        self.questionBox.delegate = self
     }
-
-
 }
 
